2535d1a5512622d048993dd68b25af5dea081597
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-01 23:08:47 +0300

    Solved 02. String Length task

---
+++ JS Advanced/Lab/01.Syntax_Functions_and_Statements/02_string_length.js
@@ -0,0 +1,6 @@
+function sovle(a, b, c) {
+    let lengthSum = a.length + b.length + c.length;
+    let averageLength = Math.floor(lengthSum / 3);
+    console.log(lengthSum);
+    console.log(averageLength);
+};
