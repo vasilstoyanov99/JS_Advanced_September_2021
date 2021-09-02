ce4881dea3ba383c90ad015a7798685bf7151d9d
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-02 11:29:40 +0300

    Solved 01. Fruit task

---
+++ JS Advanced/Exercises/01.Syntax_Functions_and_Statements/01_fruit.js
@@ -0,0 +1,7 @@
+function calculatePrice(fruit, grams, pricePerKilo) {
+    let kilos = grams / 1000.0;
+    let cost = kilos * pricePerKilo;
+    console.log(`I need $${cost.toFixed(2)} to buy ${kilos.toFixed(2)} kilograms ${fruit}.`);
+}
+
+calculatePrice('apple', 1563, 2.35);
