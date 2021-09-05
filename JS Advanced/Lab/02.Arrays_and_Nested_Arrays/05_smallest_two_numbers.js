a9d1aa88cda29c721433f8b710589d47a628a341
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-05 22:04:16 +0300

    Solved 05. Smallest Two Numbers task

--- JS Advanced/Lab/02.Arrays_and_Nested_Arrays/05_smallest_two_numbers.js
+++ JS Advanced/Lab/02.Arrays_and_Nested_Arrays/05_smallest_two_numbers.js
@@ -1,3 +1,9 @@
-function getTheTwoSmallestNumbers(params) {
+function findAndPrintTheTwoSmallestNumbers(arr) {
 
+    let sortedArr = arr.sort((a, b) => a - b);
+    let smallestNumbers = [];
+    smallestNumbers.push(sortedArr[0]);
+    smallestNumbers.push(sortedArr[1]);
+
+    console.log(smallestNumbers);
 }
