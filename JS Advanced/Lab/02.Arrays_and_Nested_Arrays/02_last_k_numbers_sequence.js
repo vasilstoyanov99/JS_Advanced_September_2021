e4c845f8a7414552ccfe56ad75d90918390c7996
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-20 13:46:27 +0300

    Create 02_last_k_numbers_sequence.js

---
+++ JS Advanced/Lab/02.Arrays_and_Nested_Arrays/02_last_k_numbers_sequence.js
@@ -0,0 +1,13 @@
+function getSequence(arrayLength, previousElements) {
+
+    //TODO: Solve it! 
+    let array = new Array(arrayLength);
+    array[0] = 1;
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+    }
+}
+
+getSequence(6, 3);
