27ea1b8a62f2ef1631a383fe694b3430f662b869
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-25 19:15:06 +0300

    Solved 02. Collect List task

--- JS Advanced/Lab/04.DOM_Introduction/02.Collect List Items/CollectListItems.js
+++ JS Advanced/Lab/04.DOM_Introduction/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,7 @@
 function extractText() {
-    // TODO
+
+    let element = document.getElementById('items');
+    let list = element.children;
+    const items = Array.from(list).map(x => x.textContent).join('\n');
+    document.getElementById('result').textContent = items;
 }
