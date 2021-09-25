aa1cf3d41d339029a7a22bc8bdb305936843540e
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-25 19:33:19 +0300

    Solved 05. Colorize Table task

--- JS Advanced/Lab/04.DOM_Introduction/05.Colorize Table/ColorizeTable.js
+++ JS Advanced/Lab/04.DOM_Introduction/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,7 @@
 function colorize() {
-    // TODO
+    let rows = document.querySelectorAll('table tr:nth-child(even)');
+
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].style.backgroundColor = 'Teal';
+    }
 }
