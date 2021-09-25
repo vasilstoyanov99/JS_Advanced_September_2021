84e9540165872100f9c5b22eee1dc84ec32ed101
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-25 19:04:56 +0300

    Solved 01. Edit Element task

--- JS Advanced/Lab/04.DOM_Introduction/01.Edit Element/EditElement.js
+++ JS Advanced/Lab/04.DOM_Introduction/01.Edit Element/EditElement.js
@@ -1,3 +1,6 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+
+    let text = element.textContent;
+    let regex = new RegExp(match, 'g');
+    element.textContent = text.replace(regex, replacer);
 }
