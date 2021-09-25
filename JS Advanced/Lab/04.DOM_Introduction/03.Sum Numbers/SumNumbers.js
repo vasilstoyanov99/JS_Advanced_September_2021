95d5fe7c06c7740f60eafcea8d3249f497dffd8e
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-25 19:19:06 +0300

    Solved 03. Sum Numbers task

--- JS Advanced/Lab/04.DOM_Introduction/03.Sum Numbers/SumNumbers.js
+++ JS Advanced/Lab/04.DOM_Introduction/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,6 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    let firstNumber = Number(document.getElementById('num1').value);
+    let secondNumber = Number(document.getElementById('num2').value);
+    const sum = firstNumber + secondNumber;
+    document.getElementById('sum').value = sum;
 }
