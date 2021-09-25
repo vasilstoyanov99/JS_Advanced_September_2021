90fcd72c81d4ceb9be0868a12214512873ba6ace
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-25 19:24:36 +0300

    Solved 04. Show More task

--- JS Advanced/Lab/04.DOM_Introduction/04.Show More/ShowMore.js
+++ JS Advanced/Lab/04.DOM_Introduction/04.Show More/ShowMore.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
+    let readMore = document.getElementById('more');
+    let text = document.getElementById('text');
+    text.style.display = 'inline';
+    readMore.style.display = 'none';
 }
