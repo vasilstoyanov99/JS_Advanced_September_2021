a202e955bbbe585ad5dc4ad04b990f03d88cf5ed
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-25 20:32:01 +0300

    Solved 07. Extract Parenthesis task

--- JS Advanced/Lab/04.DOM_Introduction/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS Advanced/Lab/04.DOM_Introduction/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,8 @@
-function extract(content) {
+function extract(id) {
+    let text = document.getElementById(id).textContent;
+    let regexWordSplit = new RegExp(/\([\w\s]+\)/, 'g');
+    let words = regexWordSplit[Symbol.match](text);
+    words = words.map(a => a.replace(/[()]/g, ''));
 
+    return words.join('; ');
 }
