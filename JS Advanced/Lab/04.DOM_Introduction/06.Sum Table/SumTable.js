3470a27dafdba26616e406fdbd2a0a1e690d910a
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-25 19:45:10 +0300

    Solved 06. Sum Table task

--- JS Advanced/Lab/04.DOM_Introduction/06.Sum Table/SumTable.js
+++ JS Advanced/Lab/04.DOM_Introduction/06.Sum Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
+    let rows = document.querySelectorAll('table tr');
+    let sum = 0;
 
+    for (let i = 1; i < rows.length - 1; i++) {
+        const cell = rows[i].lastElementChild;
+        sum += Number(cell.textContent);
+    }
+
+    document.getElementById('sum').textContent = sum;
 }
