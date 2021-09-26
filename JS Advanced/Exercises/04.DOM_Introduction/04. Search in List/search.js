201859a26af919df9c326dca1dfee02b83305e6d
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-26 15:42:57 +0300

    Solved 04. Search in List task

--- JS Advanced/Exercises/04.DOM_Introduction/04. Search in List/search.js
+++ JS Advanced/Exercises/04.DOM_Introduction/04. Search in List/search.js
@@ -1,3 +1,31 @@
 function search() {
-   // TODO
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = '';
+    let townsElementsArr = Array.from(document.getElementsByTagName('li'));
+
+    for (const element of townsElementsArr) {
+        element.style.fontWeight = '';
+        element.style.textDecoration = '';
+    }
+    
+    let searchTermElement = document.getElementById('searchText');
+    const searchTerm = searchTermElement.value;
+    let townsNamesArr = townsElementsArr.map(a => a.textContent);
+    let foundMatchesArr = [];
+    
+    for (const currWord of townsNamesArr) {
+        if (currWord.includes(searchTerm)){
+            foundMatchesArr.push(currWord);
+        }
+    }
+
+    if (foundMatchesArr.length > 0){
+        townsElementsArr.map(twe => {
+            if (foundMatchesArr.includes(twe.textContent)){
+                twe.style.fontWeight = 'bold';
+                twe.style.textDecoration = 'underline';
+            }
+        });
+    }
+    resultElement.textContent = `${foundMatchesArr.length} matches found`;
 }
