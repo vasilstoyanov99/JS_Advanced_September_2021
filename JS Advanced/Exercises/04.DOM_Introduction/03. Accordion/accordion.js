906497b428f0e4e6fe5564ad7b396569d6fb0753
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-26 14:01:51 +0300

    Solved 03. Accordion task

--- JS Advanced/Exercises/04.DOM_Introduction/03. Accordion/accordion.js
+++ JS Advanced/Exercises/04.DOM_Introduction/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    let button = document.getElementsByClassName('button')[0];
+    let extraTextElement = document.getElementById('extra');
+
+    if (button.textContent === 'More'){
+        extraTextElement.style.display = 'block';
+        button.textContent = 'Less';
+    } else if (button.textContent === 'Less'){
+        extraTextElement.style.display = 'none';
+        button.textContent = 'More';
+    }
 }
