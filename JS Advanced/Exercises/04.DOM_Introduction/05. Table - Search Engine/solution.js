2cfddb5cb5b0182c44f58f7051071e25500d902b
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-26 16:41:32 +0300

    Solved 05. Table – Search Engine task

--- JS Advanced/Exercises/04.DOM_Introduction/05. Table - Search Engine/solution.js
+++ JS Advanced/Exercises/04.DOM_Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,21 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let dataElementsArr = Array.from(document.getElementsByTagName('tbody')[0].children);
 
+      for (const row of dataElementsArr) {
+         row.classList.remove('select');
+      }
+
+      let searchTerm = document.getElementById('searchField').value;
+
+      for (let row of dataElementsArr) {
+         for (const studentData of row.getElementsByTagName('td')) {
+            if (studentData.textContent.includes(searchTerm)){
+               row.classList.add('select');
+               break;
+            }
+         }
+      }
    }
 }
