b1cdf2548788a39297d782c99249bbf11bc03f18
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-26 13:52:58 +0300

    Solved 02. Pascal or Camel Case task

--- JS Advanced/Exercises/04.DOM_Introduction/02. Pascal or Camel Case/solution.js
+++ JS Advanced/Exercises/04.DOM_Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-  //TODO...
+    let words = document.getElementById('text').value;
+    let namingConvention = document.getElementById('naming-convention').value;
+    let wordsArr = words.split(' ').map(a => a.toLowerCase());
+    let result = [];
+
+    if (namingConvention === 'Pascal Case'){
+        result = wordsArr.map(currWord => {
+            return currWord.charAt(0).toUpperCase() + currWord.slice(1);
+        });
+    } else if (namingConvention === 'Camel Case'){
+        result = wordsArr.map((currWord, i) => {
+            if (i !== 0){
+                return currWord.charAt(0).toUpperCase() + currWord.slice(1);
+            }
+            return currWord;
+        });
+    } else{
+        result.push('Error!');
+    }
+
+    document.getElementById('result').textContent = result.join('');
 }
