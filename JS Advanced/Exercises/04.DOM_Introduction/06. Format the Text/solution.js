0eb25340902810c6317a2217b1dd25577da6644f
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-27 19:54:58 +0300

    Solved 06. Format the Text task

--- JS Advanced/Exercises/04.DOM_Introduction/06. Format the Text/solution.js
+++ JS Advanced/Exercises/04.DOM_Introduction/06. Format the Text/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-  //TODO
+    let text = document.getElementById('input').value;
+    let sentencesArr = text.split('.')
+        .filter(a => a !== '')
+        .map(a => a + '.');
+
+    let totalParagraphsRoof = Math.ceil(sentencesArr.length / 3);
+    let outputElement = document.getElementById('output');
+
+    for (let i = 0; i < totalParagraphsRoof; i++) {
+        outputElement.innerHTML += `<p>${sentencesArr.splice(0, 3).join('')}</p>`;
+    }
 }
