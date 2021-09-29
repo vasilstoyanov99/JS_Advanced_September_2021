b9964ce8055bd8fe760388c7a18eecce18bb4676
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-29 20:50:21 +0300

    Solved 03. Add / Delete task

--- JS Advanced/Lab/05._DOM_Events/03. Add-Delete/app.js
+++ JS Advanced/Lab/05._DOM_Events/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    const inputElement = document.getElementById('newItemText');
+    const text = inputElement.value;
+    const newLi = document.createElement('li');
+    newLi.textContent = text;
+
+    const deleteButton = document.createElement('a');
+    deleteButton.href = '#';
+    deleteButton.textContent = '[Delete]';
+    deleteButton.addEventListener('click', removeElement);
+    newLi.appendChild(deleteButton);
+    document.getElementById('items').appendChild(newLi);
+
+    inputElement.value = '';
+
+    function removeElement(ev) {
+        ev.target.parentElement.remove();
+    }
 }
