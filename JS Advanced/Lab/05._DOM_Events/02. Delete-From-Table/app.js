63d02f67b661f0cfe87f62e914e25739ec6778e5
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-29 20:22:46 +0300

    Solved 02. Delete from Table task

--- JS Advanced/Lab/05._DOM_Events/02. Delete-From-Table/app.js
+++ JS Advanced/Lab/05._DOM_Events/02. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const searchTerm = document.querySelector('input[name="email"]').value;
+    const elements = Array.from(document.querySelectorAll('td'))
+        .filter(a => {
+            return a.textContent === searchTerm;
+        });
+
+    let isElementRemoved = false;
+    for (const element of elements) {
+        element.parentElement.remove();
+        isElementRemoved = true;
+    }
+
+    const message = isElementRemoved ? 'Deleted.' : 'Not found.';;
+    document.getElementById('result').textContent = message;
 }
