c1cfd2f55cd955063180e745fcd9465385a6040a
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-09-29 21:35:14 +0300

    Solved 05. Highlight Active task

--- JS Advanced/Lab/05._DOM_Events/05. Highlight-Active/app.js
+++ JS Advanced/Lab/05._DOM_Events/05. Highlight-Active/app.js
@@ -1,3 +1,9 @@
 function focused() {
-    console.log('TODO:...');
+    Array.from(document.getElementsByTagName('input'))
+        .forEach(e => {
+           e.addEventListener
+           ('focus', (ev) => ev.target.parentElement.classList.add('focused'));
+           e.addEventListener
+           ('blur', (ev) => ev.target.parentElement.classList.remove('focused'));
+        });
 }
