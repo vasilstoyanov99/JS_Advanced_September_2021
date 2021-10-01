0eefa18fac2c2dc711f9751dd14049d463e08e1d
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-01 22:22:41 +0300

    Solved 02. Time Converter task

--- JS Advanced/Exercises/05.DOM_Events/02. Time-Converter/solution.js
+++ JS Advanced/Exercises/05.DOM_Events/02. Time-Converter/solution.js
@@ -1,4 +1,43 @@
 function attachEventsListeners() {
+    let main = document.getElementsByTagName('main')[0]
+        .addEventListener('click', calculateOnClick);
+    let daysElement = document.getElementById('days');
+    let hoursElement = document.getElementById('hours');
+    let minutesElement = document.getElementById('minutes');
+    let secondsElement = document.getElementById('seconds');
 
-    console.log('TODO:...');
+    function calculateOnClick({target}) {
+        if (target.type === 'button'){
+            const timeType = target.parentElement.firstElementChild.htmlFor;
+            const inputValue = Number(target.parentElement.children[1].value);
+
+
+            switch (timeType){
+                case 'days':{
+                    hoursElement.value = inputValue * 24;
+                    minutesElement.value = inputValue * 1440;
+                    secondsElement.value = inputValue * 86400;
+                    break;
+                }
+                case 'hours': {
+                    daysElement.value = inputValue / 24;
+                    minutesElement.value = inputValue * 60;
+                    secondsElement.value = inputValue * 3600;
+                    break;
+                }
+                case 'minutes': {
+                    daysElement.value = inputValue / 1440;
+                    hoursElement.value = inputValue / 60;
+                    secondsElement.value = inputValue * 60;
+                    break;
+                }
+                case 'seconds': {
+                    daysElement.value = inputValue / 86400;
+                    hoursElement.value = inputValue / 3600;
+                    minutesElement.value = inputValue / 60;
+                    break;
+                }
+            }
+        }
+    }
 }
