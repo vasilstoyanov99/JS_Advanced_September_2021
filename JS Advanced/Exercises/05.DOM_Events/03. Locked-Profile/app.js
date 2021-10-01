22b44f7cbfae154c70549c4a89943639400b7421
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-02 00:02:58 +0300

    Solved 03. Locked Profile task

--- JS Advanced/Exercises/05.DOM_Events/03. Locked-Profile/app.js
+++ JS Advanced/Exercises/05.DOM_Events/03. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+    document.getElementById('main')
+        .addEventListener('click', onClick);
+
+    function onClick({target}) {
+        if (target.tagName === 'BUTTON'){
+            const radioButton = target.parentNode.querySelector('input[value="unlock"]');
+
+            if (radioButton.checked){
+                let button = target.parentNode.querySelector('button');
+                const buttonText = button.textContent;
+                const showMore = 'Show more';
+                const hideIt = 'Hide it';
+                let hiddenFiled = button.parentElement.querySelector('div');
+
+                if (buttonText === showMore){
+                    hiddenFiled.style.display = 'block';
+                    button.textContent = hideIt;
+                    } else if (buttonText === hideIt){
+                    hiddenFiled.style.display = 'none';
+                    button.textContent = showMore;
+                }
+            }
+        }
+    }
 }
