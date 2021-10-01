8aa124ac1b6910b81b79c597b91dadfb2e6dcded
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-02 02:08:31 +0300

    Solved 04. Fill Dropdown task

--- JS Advanced/Exercises/05.DOM_Events/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/Exercises/05.DOM_Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    const itemTextElement = document.getElementById('newItemText');
+    const itemValueElement = document.getElementById('newItemValue');
+    const selectElement = document.getElementById('menu');
+
+    let option = document.createElement('option');
+    option.textContent = itemTextElement.value;
+    option.value = itemValueElement.value;
+    selectElement.appendChild(option);
+
+    itemTextElement.value = '';
+    itemValueElement.value = '';
 }
