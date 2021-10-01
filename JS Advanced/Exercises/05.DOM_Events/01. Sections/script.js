b53dd3bb43963c6a5c52fa62d7700a154e4c2937
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-01 21:14:11 +0300

    Solved 01. Sections task

--- JS Advanced/Exercises/05.DOM_Events/01. Sections/script.js
+++ JS Advanced/Exercises/05.DOM_Events/01. Sections/script.js
@@ -1,3 +1,26 @@
 function create(words) {
-   console.log('TODO:...');
+   let contentElement = document.getElementById('content');
+
+   for (const word of words) {
+      let div = createDivWithParagraph(word);
+      contentElement.appendChild(div);
+   }
+
+   contentElement.addEventListener('click', onClick);
+
+   function onClick({target}) {
+      let paragraph = target.firstChild;
+      if (paragraph.tagName === 'P'){
+         paragraph.style.display = 'block';
+      }
+   }
+
+   function createDivWithParagraph(word) {
+      let paragraph = document.createElement('p');
+      paragraph.textContent = word;
+      paragraph.style.display = 'none';
+      let div = document.createElement('div');
+      div.appendChild(paragraph);
+      return div;
+   }
 }
