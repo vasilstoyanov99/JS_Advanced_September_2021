b3b5089f636d803e8667369c80a01b4f0ff823fa
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-02 02:28:03 +0300

    Solved 05. Encode and Decode Messages task

--- JS Advanced/Exercises/05.DOM_Events/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/Exercises/05.DOM_Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,33 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    document.querySelector('button')
+        .addEventListener('click', encodeAndSend);
+    const outputTextArea = document.querySelectorAll('textarea')[1];
+    document.querySelectorAll('button')[1]
+        .addEventListener('click', decode);
+
+    function encodeAndSend({target}) {
+        const inputTextArea = target.previousElementSibling;
+        const message = inputTextArea.value;
+        let encodedMessage = '';
+
+        for (let i = 0; i < message.length; i++) {
+            let newAscii = message.charCodeAt(i) + 1;
+            encodedMessage += String.fromCharCode(newAscii);
+        }
+
+        inputTextArea.value = '';
+        outputTextArea.value = encodedMessage;
+    }
+
+    function decode() {
+        const encodedMessage = outputTextArea.value;
+        let decodedMessage = '';
+
+        for (let i = 0; i < encodedMessage.length; i++) {
+            let newAscii = encodedMessage.charCodeAt(i) - 1;
+            decodedMessage += String.fromCharCode(newAscii);
+        }
+
+        outputTextArea.value = decodedMessage;
+    }
 }
