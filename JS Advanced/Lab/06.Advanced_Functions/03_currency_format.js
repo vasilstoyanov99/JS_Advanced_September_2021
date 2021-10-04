fadc14716132ce7084bb08d7a5050a3188ead763
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-05 01:44:37 +0300

    Update 03_currency_format.js

--- JS Advanced/Lab/06.Advanced_Functions/03_currency_format.js
+++ JS Advanced/Lab/06.Advanced_Functions/03_currency_format.js
@@ -11,8 +11,3 @@ function currencyFormatter(separator, symbol, symbolFirst, value) {
     else return result + ' ' + symbol;
 }
 
-let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
-console.log(dollarFormatter(5345));   // $ 5345,00
-console.log(dollarFormatter(3.1429)); // $ 3,14
-console.log(dollarFormatter(2.709));  // $ 2,71
-
