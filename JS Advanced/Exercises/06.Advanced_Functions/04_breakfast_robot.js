e1e6ecc88fa227b3657e6a012f197fc0fc8f4ccb
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-05 14:46:09 +0300

    Solved 04. Breakfast Robot task

---
+++ JS Advanced/Exercises/06.Advanced_Functions/04_breakfast_robot.js
@@ -0,0 +1,40 @@
+function robot() {
+    const successMessage = 'Success';
+    let availableIngredients = { protein: 0, carbohydrate: 0, fat: 0, flavour: 0
+    };
+    let recipes = {
+        apple: { carbohydrate: 1, flavour: 2},
+        lemonade: { carbohydrate: 10, flavour: 20 },
+        burger: { carbohydrate: 5, fat: 7, flavour: 3},
+        eggs: { protein: 5, fat: 1, flavour: 1},
+        turkey: { protein: 10, carbohydrate: 10, fat: 10, flavour: 10},
+        prepareRecipe(recipe, quantity) {
+            for (const ingredient in this[recipe]) {
+                const neededIngredient = this[recipe][ingredient] * quantity;
+                if (neededIngredient > availableIngredients[ingredient] ){
+                    return (`Error: not enough ${ingredient} in stock`);
+                }
+                availableIngredients[ingredient] -= neededIngredient;
+            }
+            return successMessage;
+        }
+    };
+
+    function process(input) {
+        const [command, ingredientOrRecipe, quantity] = input.split(' ');
+
+        if (command === 'restock') {
+            availableIngredients[ingredientOrRecipe] += Number(quantity);
+            return successMessage;
+        } else if(command === 'prepare') {
+            return recipes.prepareRecipe(ingredientOrRecipe, Number(quantity));
+        } else {
+            const result = Object.keys(availableIngredients)
+                .map(k => `${k}=${availableIngredients[k]}`)
+                .join(' ');
+            return  result;
+        }
+    }
+
+    return process;
+}
