5cc495e2ee7175c9476b78776825141998651aae
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-07 00:51:37 +0300

    Update solution.js

--- JS Advanced/Exercises/04.DOM_Introduction/06. Format the Text/solution.js
+++ JS Advanced/Exercises/04.DOM_Introduction/06. Format the Text/solution.js
@@ -1,4 +1,4 @@
-function solve() {
+/*function solve() {
     let text = document.getElementById('input').value;
     let sentencesArr = text.split('.')
         .filter(a => a)
@@ -9,5 +9,30 @@ function solve() {
 
     for (let i = 0; i < totalParagraphsRoof; i++) {
         outputElement.innerHTML += `<p>${sentencesArr.splice(0, 3).join('')}</p>`;
+    }
+}*/
+
+function solve() {
+    const text = document.getElementById('input').value;
+    let textInArray = text.split('.');
+    let paragraphsCount = 0;
+    let paragraphs = [];
+    let output = document.getElementById('output');
+
+    for (sentance of textInArray) {
+        if (sentance == '' || sentance.length < 1) {
+            textInArray.pop(sentance);
+        }
+    }
+
+    paragraphsCount = Math.ceil(textInArray.length / 3);
+
+    for (let i = 1; i <= paragraphsCount; i++) {
+        paragraphs.push(textInArray.splice(0, 3));
+    }
+
+    for (let i = 0; i < paragraphs.length; i++) {
+        let pText = paragraphs[i].join('. ');
+        output.innerHTML += `<p>${pText}\n</p>`;
     }
 }
