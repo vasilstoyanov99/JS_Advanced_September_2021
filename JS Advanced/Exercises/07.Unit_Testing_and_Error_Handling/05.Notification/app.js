47dc0eedbb7e2636351f0d48a6b2ad8bdfd074e3
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-10 03:48:21 +0300

    Solved 05. Notification task

--- JS Advanced/Exercises/07.Unit_Testing_and_Error_Handling/05.Notification/app.js
+++ JS Advanced/Exercises/07.Unit_Testing_and_Error_Handling/05.Notification/app.js
@@ -1,4 +1,10 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+  const notificationDiv = document.getElementById('notification');
+  notificationDiv.textContent = message;
+  notificationDiv.style.display = 'block';
+  notificationDiv.addEventListener('click', hideNotification);
+
+  function hideNotification({target}) {
+    target.style.display = 'none';
+  }
 }
