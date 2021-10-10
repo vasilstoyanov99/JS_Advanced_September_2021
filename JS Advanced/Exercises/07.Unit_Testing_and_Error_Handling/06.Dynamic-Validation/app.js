dc2d5edb7b0f5415ad1fce50bfcf82cc3564ef2c
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-10 03:52:42 +0300

    Solved 06. Dynamic Validation task

--- JS Advanced/Exercises/07.Unit_Testing_and_Error_Handling/06.Dynamic-Validation/app.js
+++ JS Advanced/Exercises/07.Unit_Testing_and_Error_Handling/06.Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+    const emailInputElement = document.getElementById('email');
+    emailInputElement.addEventListener('change', validateEmail);
+    const regex = new RegExp(/^[a-z]+@[a-z]+\.[a-z]+$/);
+
+    function validateEmail(ev) {
+        if (!regex[Symbol.match](ev.target.value)){
+            emailInputElement.classList.add('error');
+        } else {
+            emailInputElement.classList.remove('error');
+        }
+    }
 }
