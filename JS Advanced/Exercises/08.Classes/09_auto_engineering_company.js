c4d068080ef2a28f4eb63489015353c1c66045ff
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-14 16:44:19 +0300

    Update 09_auto_engineering_company.js

--- JS Advanced/Exercises/08.Classes/09_auto_engineering_company.js
+++ JS Advanced/Exercises/08.Classes/09_auto_engineering_company.js
@@ -36,14 +36,3 @@ function registerCarSales(data) {
         console.log(`###${carModel} -> ${totalProducedCars}`);
     }
 }
-
-registerCarSales(['Mercedes-Benz | 50PS | 123',
-    'Mini | Clubman | 20000',
-    'Mini | Convertible | 1000',
-    'Mercedes-Benz | 60PS | 3000',
-    'Hyunday | Elantra GT | 20000',
-    'Mini | Countryman | 100',
-    'Mercedes-Benz | W210 | 100',
-    'Mini | Clubman | 1000',
-    'Mercedes-Benz | W163 | 200']
-);
