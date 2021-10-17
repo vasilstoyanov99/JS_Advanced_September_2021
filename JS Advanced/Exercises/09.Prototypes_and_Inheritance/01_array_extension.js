b6047ca5418bf1ae97bcc9b0d8e67e12ce97f6eb
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-17 07:31:39 +0300

    Solved 01. Array Extension task

---
+++ JS Advanced/Exercises/09.Prototypes_and_Inheritance/01_array_extension.js
@@ -0,0 +1,36 @@
+(function extendArrayPrototype() {
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    }
+
+    Array.prototype.skip = function (count) {
+        let result = [];
+
+        for (let i = count; i < this.length; i++) {
+            result.push(this[i]);
+        }
+
+        return result;
+    }
+
+    Array.prototype.take = function (count) {
+        let result = [];
+
+        for (let i = 0; i <= count; i++) {
+            result.push(this[i]);
+        }
+
+        return result;
+    }
+
+    Array.prototype.sum = function () {
+        return this.reduce((acc, e) => acc += e);
+    }
+
+    Array.prototype.average = function () {
+        return this.sum() / this.length;
+    }
+})()
+
+var testArray = [1, 2, 3];
+console.log(testArray.average());
