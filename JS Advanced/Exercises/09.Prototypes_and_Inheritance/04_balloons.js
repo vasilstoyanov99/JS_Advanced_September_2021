5b8e06988e7ea4d6b660a25bd94de9e5556209d0
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-18 03:01:22 +0300

    Solved 04. Balloons task

---
+++ JS Advanced/Exercises/09.Prototypes_and_Inheritance/04_balloons.js
@@ -0,0 +1,42 @@
+function solve() {
+    class Balloon {
+        constructor(colorInput, hasWeightInput) {
+            this.color = colorInput;
+            this.hasWeight = hasWeightInput;
+        }
+    }
+
+    class PartyBalloon extends Balloon {
+        constructor(colorInput, hasWeightInput,
+                    ribbonColorInput, ribbonLengthInput) {
+            super(colorInput, hasWeightInput);
+            this._ribbon = {
+                color: ribbonColorInput,
+                length: ribbonLengthInput
+            };
+        }
+
+        get ribbon() {
+            return this._ribbon;
+        }
+    }
+
+    class BirthdayBalloon extends PartyBalloon {
+        constructor(colorInput, hasWeightInput,
+                    ribbonColorInput, ribbonLengthInput, textInput) {
+            super(colorInput, hasWeightInput,
+                ribbonColorInput, ribbonLengthInput);
+            this._text = textInput;
+        }
+
+        get text() {
+            return this._text;
+        }
+    }
+
+    return {
+        Balloon: Balloon,
+        PartyBalloon: PartyBalloon,
+        BirthdayBalloon: BirthdayBalloon
+    }
+}
