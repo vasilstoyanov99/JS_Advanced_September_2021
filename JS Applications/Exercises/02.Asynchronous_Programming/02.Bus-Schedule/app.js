5cd8a97538d3cf8ac45a790a889e2ee3627c7e4d
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-10-31 03:37:29 +0300

    Solved 02. Bus Schedule task

--- JS Applications/Exercises/02.Asynchronous_Programming/02.Bus-Schedule/app.js
+++ JS Applications/Exercises/02.Asynchronous_Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,35 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const infoEl = document.getElementById('info');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    let stopData = {
+        name: '',
+        next: 'depot'
+    };
+
+    async function depart() {
+        try {
+            departBtn.disabled = true;
+            const url = `http://localhost:3030/jsonstore/bus/schedule/${stopData.next}`;
+            const response = await fetch(url);
+
+            if (response.status !== 200) {
+                throw new Error('Stop is not found!');
+            }
+            stopData = await response.json();
+            infoEl.textContent = `Next stop ${stopData.name}`;
+            arriveBtn.disabled = false;
+
+        } catch (error) {
+            infoEl.textContent = 'Error';
+        }
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        arriveBtn.disabled = true;
+        infoEl.textContent = `Arriving at ${stopData.name}`;
+        departBtn.disabled = false;
     }
 
     return {
