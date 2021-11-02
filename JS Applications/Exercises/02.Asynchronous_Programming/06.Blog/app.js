8e87231c7f1210fb90c9544f0d26c6c5ff38d6c5
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-11-02 02:47:27 +0200

    Solved 06. Blog task

--- JS Applications/Exercises/02.Asynchronous_Programming/06.Blog/app.js
+++ JS Applications/Exercises/02.Asynchronous_Programming/06.Blog/app.js
@@ -1,5 +1,71 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoadPosts')
+        .addEventListener('click', loadPosts);
+    document.getElementById('btnViewPost')
+        .addEventListener('click', loadPostData);
+
+    async function loadPosts() {
+        const data = await fetchAndReturnData('http://localhost:3030/jsonstore/blog/posts');
+        const postsSelect = document.getElementById('posts');
+
+        for (let key in data) {
+            const postData = data[key];
+            const post =
+                createStructure('option', 'value', postData.id, postData.title);
+            postsSelect.appendChild(post);
+        }
+    }
+
+    async function loadPostData() {
+        const allPosts = document.querySelectorAll('option');
+        const selectedPost = Array.from(allPosts).filter(p => p.selected === true)[0];
+        const id = selectedPost.value;
+        const commentsData = await fetchAndReturnData('http://localhost:3030/jsonstore/blog/comments');
+        const selectedPostComments = [];
+
+        for (let key in commentsData) {
+            const commentData = commentsData[key];
+            if (commentData.postId === id) {
+                selectedPostComments.push({
+                    id: commentData.id,
+                    text: commentData.text
+                });
+            }
+        }
+
+        const postData = await fetchAndReturnData
+        (`http://localhost:3030/jsonstore/blog/posts/${id}`);
+        document.getElementById('post-title').textContent = postData.title;
+        document.getElementById('post-body').textContent = postData.body;
+        const commentsUl = document.getElementById('post-comments');
+        commentsUl.replaceChildren();
+        selectedPostComments.forEach(c => {
+            const commentLi = createStructure('li', 'id', c.id, c.text);
+            commentsUl.appendChild(commentLi);
+        });
+    }
+
+    function createStructure(type, attribute, value, content) {
+        const element = document.createElement(type);
+        element[attribute] = value;
+        element.textContent = content;
+
+        return element;
+    }
+
+    async function fetchAndReturnData(url) {
+        try {
+            let response = await fetch(url);
+
+            if (response.status !== 200) {
+                throw new Error('Error');
+            }
+            return await response.json();
+
+        } catch (error) {
+            alert(error.message);
+        }
+    }
 }
 
 attachEvents();
